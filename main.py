3fd7de7915e072c235fe8c409adbdd15b0b0f055
Vinícius Moraes <viniciuseduardo1609@gmail.com>
2026-04-26 16:18:12 -0300

    code comments correction

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ lr = 0.01
 epochs = 500
 
 network = Network(layers=[2, 4, 3, 1], internal_act="relu", last_act="relu")
+for layer in network.layers:
+    for e, neuron in enumerate(layer.neurons):
+        print(f"neuron {e}: {neuron.weights}  |  {neuron.b}")
+    print("--------------------------------------------------")
 loss_history = network.train_example([1, 2], [10], 0.01)
-
-
+for layer in network.layers:
+    for e, neuron in enumerate(layer.neurons):
+        print(f"neuron {e}: {neuron.weights}  |  {neuron.b}")
